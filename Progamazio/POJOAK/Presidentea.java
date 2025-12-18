9af79b9053e40a216ae72f28f9788df372c9938b
Hector <364610F@H015110.NET>
2025-12-18 14:10:44 +0100

    Klase diagramaren bertsio berria eta pojoak

---
+++ Progamazio/POJOAK/Presidentea.java
@@ -0,0 +1,31 @@
+package Prueba_De_Pojos;
+
+import java.util.ArrayList; 
+
+public class Presidentea extends Pertsona {
+
+	private ArrayList<Pertsona> taldea;
+	
+	public Presidentea(String izena, String abz1, int adina) {
+		super(izena, abz1, adina);
+		this.taldea = new ArrayList<>();
+	}
+
+	public ArrayList<Pertsona> getTaldea() {
+		return taldea;
+	}
+
+	public void setTaldea(ArrayList<Pertsona> taldea) {
+		this.taldea = taldea;
+	}
+
+	@Override
+	public String toString() {
+		return "Presidentea [taldea=" + taldea + ", izena=" + izena + ", abz1=" + abz1 + ", adina=" + adina + "]";
+	}
+
+
+	
+	
+	
+}
